3d5a62b02e6dd76571191e78cae5f6bf95523c00
MandyJavan <javanbakhtmeh@gmail.com>
2024-04-13 02:53:37 +0330

    Some features have been done!

---
+++ script.js
@@ -0,0 +1,78 @@
+let cookiesCondition = document.getElementById("cookiesCondition")
+let accept = document.querySelector("#accept")
+let leaveSite = document.querySelector("#leaveSite")
+let myBody = window.body;
+let signup = document.querySelector("#signup");
+let signin = document.querySelector("#signin");
+let loginSection = document.querySelector("#loginSection")
+let forgotPwd = document.querySelector("#forgotPwd");
+let closeSignin = document.querySelector("#closeSignin")
+let profile = document.querySelector("#profile")
+let profileIsOpen = false;
+let aside = document.querySelector("aside");
+let chatLink = document.querySelector("#chatLink")
+let chatPhoto = document.querySelector("#chatholder")
+let chatPhoto_hover = false;
+let profileField = document.querySelector("#profileField")
+
+
+accept.addEventListener("click", ()=> {
+    cookiesCondition.classList.toggle("bg-success")
+    cookiesCondition.firstElementChild.textContent = "Thank you for accepting our policies!"
+    cookiesCondition.firstElementChild.style.color = "white"
+    accept.style.display = "none"
+    leaveSite.style.display = "none"
+    setTimeout(()=> {
+        cookiesCondition.classList.toggle("d-none")
+    }, 2000)
+})
+
+leaveSite.addEventListener("click", ()=> {
+    cookiesCondition.style.backgroundColor = "red"
+    cookiesCondition.firstElementChild.textContent = "Hope to see you soon again!"
+    cookiesCondition.firstElementChild.style.color = "white"
+    accept.style.display = "none"
+    leaveSite.style.display = "none"
+    setTimeout(()=> {
+        myBody.classList.toggle = "bg-warning"
+        alert("Bye!")
+    }, 1000)
+})
+
+signin.addEventListener("click", ()=> {
+    signin.style.display = "none"
+    signup.style.display = "none"
+    loginSection.style.display = "flex"
+    forgotPwd.style.display = "block"
+})
+
+closeSignin.addEventListener("click", ()=> {
+    loginSection.style.display = "none"
+    signin.style.display = "initial"
+    signup.style.display = "initial"
+})
+
+profile.addEventListener("click", ()=> {
+    profileIsOpen = !profileIsOpen
+    if (profileIsOpen) {
+        aside.style.width = "480px"
+        profile.textContent = "Profile v"
+        profileField.style.display = "flex"
+    } else {
+        aside.style.width = "120px"
+        profile.textContent = "Profile >"
+        profileField.style.display = "none"
+    }
+})
+
+chatPhoto.addEventListener('mouseover', () => {
+    chatPhoto_hover = true
+    if (chatPhoto_hover) {
+        chatLink.style.right = "70px";
+    }
+})
+
+chatPhoto.addEventListener("mouseout", ()=> {
+    chatPhoto_hover = false;
+    chatLink.style.right = "-200px"
+})
